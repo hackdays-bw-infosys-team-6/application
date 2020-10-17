551fb6b0a1cef2630defd2095e0e73988f2e9723
Kristjan Liiva <kristjan.liiva@caruso-dataplace.com>
2020-10-17 22:04:00 +0200

    Include switching between current and future profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">Profile</h1>
+      <div>
+        <button @click="showFutureProfile = false">Current Profile</button>
+        <button @click="showFutureProfile = true">Future Profile</button>
+      </div>
+
+      <div>
+        <img src="https://image.shutterstock.com/image-photo/portrait-mature-blond-man-smiling-600w-682038574.jpg" alt="profile" class="profile-image">
+        <p>{{profile.name}}</p>
+        <p>{{profile.email}}</p>
+      </div>
 
       <div class="profile-section">
         <h2>WORK EXPERIENCE</h2>
@@ -14,6 +23,9 @@
         <h2>SKILLS</h2>
         <p>
           <span v-for="skill in profile.skills" v-bind:key="skill">{{skill}}</span>
+          <template v-if="showFutureProfile">
+            <span v-for="skill in profile.futureSkills" v-bind:key="skill">{{skill}}</span>
+          </template>
         </p>
       </div>
 
@@ -21,6 +33,9 @@
         <h2>EDUCATION</h2>
         <ul>
           <li v-for="education in profile.education" v-bind:key="education">{{education}}</li>
+          <template v-if="showFutureProfile">
+            <li v-for="education in profile.futureEducation" v-bind:key="education">{{education}}</li>
+          </template>
         </ul>
       </div>
 
@@ -30,7 +45,6 @@
           <li v-for="job in profile.suggestedJobs" v-bind:key="job.title">{{job.title}}</li>
         </ul>
       </div>
-
     </div>
 
     <Navigation />
@@ -43,42 +57,48 @@ import Vue from 'vue'
 export default Vue.extend({
   data: function () {
     return {
+      showFutureProfile: false,
       profile: {
-      name: 'Thomas Müller',
-      email: 'thomas.m@hotmail.com',
-      workexperience: [
-        {
-          description: 'Senior Machine Operator | Schäffler | 2005 - 2020'
-        },
-        {
-          description: 'Technician Apprenticeship | FAG | 2002 - 2005'
-        }
-      ],
-      skills: [
-        'G-code programming', 'CNC programming', 'Quality Control', 'Maintenance & Repair', 'Welding', 'Plan Management'
-      ],
-      futureSkills: [
-        'Rapid tooling', '3D printing', 'Adaptive Manufacturing'
-      ],
-      education: [
-        'Vocational training in machine technology'
-      ],
-      futureEducation: [
-        'Generative Design for Additive Manufacturing',
-        'Rapid Manufacturing',
-        'Designing for 3D Printing with Fusion 360',
-      ],
-      suggestedJobs: [
-        {
-          title: 'Additive Manufacturing Technican',
-          company: 'Tesla',
-          logo: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg',
-          location: 'Berlin',
-          posted: '3 days ago',
-          liked: false
-        }
-      ]
+        name: 'Thomas Müller',
+        email: 'thomas.m@hotmail.com',
+        workexperience: [
+          {
+            description: 'Senior Machine Operator | Schäffler | 2005 - 2020'
+          },
+          {
+            description: 'Technician Apprenticeship | FAG | 2002 - 2005'
+          }
+        ],
+        skills: [
+          'G-code programming', 'CNC programming', 'Quality Control', 'Maintenance & Repair', 'Welding', 'Plan Management'
+        ],
+        futureSkills: [
+          'Rapid tooling', '3D printing', 'Adaptive Manufacturing'
+        ],
+        education: [
+          'Vocational training in machine technology'
+        ],
+        futureEducation: [
+          'Generative Design for Additive Manufacturing',
+          'Rapid Manufacturing',
+          'Designing for 3D Printing with Fusion 360',
+        ],
+        suggestedJobs: [
+          {
+            title: 'Additive Manufacturing Technican',
+            company: 'Tesla',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg',
+            location: 'Berlin',
+            posted: '3 days ago',
+            liked: false
+          }
+        ]
+      }
     }
+  },
+  methods: {
+    setShowFutureProfile: function(show: boolean) {
+      this.showFutureProfile = show;
     }
   }
 })
@@ -87,5 +107,11 @@ export default Vue.extend({
 <style>
 .profile-section {
   margin-top: 32px;
+}
+
+.profile-image {
+  border-radius: 50%;
+  height: 60px;
+  width: 60px;
 }
 </style>
